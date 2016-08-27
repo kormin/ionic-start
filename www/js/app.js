5a28db197059cdb592d261db1c655092c3d04a6c
kormin <abaotom14@gmail.com>
2016-08-27 12:10:44 +0800

    add task working

--- www/js/app.js
+++ www/js/app.js
@@ -23,11 +23,41 @@ angular.module('todo', ['ionic'])
   });
 })
 
-.controller('TodoCtrl', function($scope) {
+.controller('TodoCtrl', function($scope, $ionicModal) {
+  // No need for testing data anymore
   $scope.tasks = [
+    // default values
     { title: 'Collect coins' },
     { title: 'Eat mushrooms' },
     { title: 'Get high enough to grab the flag' },
     { title: 'Find the Princess' }
   ];
+
+  // Create and load the Modal
+  $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+    $scope.taskModal = modal;
+  }, {
+    scope: $scope,
+    animation: 'slide-in-up'
+  });
+
+  // Called when the form is submitted
+  $scope.createTask = function(task) {
+    $scope.tasks.push({
+      title: task.title
+    });
+    $scope.taskModal.hide();
+    task.title = "";
+  };
+
+  // Open our new task modal
+  $scope.newTask = function() {
+    $scope.taskModal.show();
+  };
+
+  // Close the new task modal
+  $scope.closeNewTask = function() {
+    $scope.taskModal.hide();
+  };
 })
+
